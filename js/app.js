4946e93b4a055eabb455192a2be8fcdca11f0257
Brian Barthold <brian@grossbauer.com>
2018-04-01 19:28:26 -0500

    setup prev and next buttons
    Allow users to browser when modal window is up

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ fetch('https://randomuser.me/api?&inc=gender,name,picture,email,location,phone,c
     //console.log(myJson);
     for (var i = 0; i < 12; i++) {
         var name = data.results[i]['name']['first'] + " " + data.results[i]['name']['last'];
+        document.querySelectorAll('.block')[i].setAttribute("data-employeenumber", i); 
         document.querySelectorAll('.block h2')[i].innerHTML = name;
         document.querySelectorAll('.block img')[i].src = data.results[i]['picture']['medium'];
         document.querySelectorAll('.block a')[i].innerHTML = data.results[i]['email'];
@@ -27,24 +28,83 @@ document.querySelector('.panel__main').addEventListener('click', function(evt){
       evt.target && evt.target.nodeName === "P"
       ) {                
         parentBlock = evt.target.parentNode.parentNode;
-    }
-
-    if(evt.target && evt.target.nodeName === "A") {      
+    } else if(evt.target && evt.target.nodeName === "A") {      
         parentBlock = evt.target.parentNode.parentNode.parentNode;
+    } else if(evt.target && evt.target.nodeName === "IMG") {      
+        parentBlock = evt.target.parentNode;
+    } else if (evt.target && evt.target.className=="block__wrapper") {      
+        parentBlock = evt.target.children[0];                      
+    } else if (evt.target && evt.target.className=="block") {              
+        parentBlock = evt.target;              
     }
 
-    if(evt.target && evt.target.nodeName === "IMG") {      
-        parentBlock = evt.target.parentNode;
-    }    
+    // load employee data now in modal
+    populateModal(parentBlock);
+    
+});   
 
-    const img = parentBlock.firstElementChild.src;    
-    const city = parentBlock.querySelector('.city').innerHTML;
-    const name = parentBlock.querySelector('.name').innerHTML;
-    const email = parentBlock.querySelector('.email').innerHTML;    
-    const phone = parentBlock.querySelector('.phone').innerHTML;    
-    const address = parentBlock.querySelector('.address').innerHTML;    
-    const birthday = parentBlock.querySelector('.birthday').innerHTML;    
+document.querySelector('.next').addEventListener('click', function(evt){
+    evt.preventDefault();
+    evt.stopPropagation();
+    
+    var parentId = document.querySelectorAll('.modal')[0].getAttribute("data-parent");
+    
+    if (parentId < 11) {
+        parentId = parseInt(parentId) + 1;    
+    } else {
+        parentId = 0;    
+    }
 
+    locateEmployeeData(parentId);
+    
+});
+
+document.querySelector('.prev').addEventListener('click', function(evt){
+    evt.preventDefault();
+    evt.stopPropagation();
+    
+    var parentId = document.querySelectorAll('.modal')[0].getAttribute("data-parent");
+
+    if (parentId > 0) {
+        parentId = parseInt(parentId) - 1;    
+    } else {
+        parentId = 11;    
+    }
+    locateEmployeeData(parentId);
+});
+
+document.querySelector('.modal__wrapper').addEventListener('click', function(evt){
+    evt.preventDefault();
+    this.className = "modal__wrapper";    
+});    
+
+/**
+ * [gatherEmployeeData description]
+ * @return {[type]} [description]
+ */
+function locateEmployeeData(parentId) {
+    let getEmployee = document.querySelectorAll('[data-employeenumber="'+parentId+'"]');
+    let employee = getEmployee[0];    
+    populateModal(employee);
+}
+
+/**
+ * [getEmployeeData description]
+ * @return {[type]} [description]
+ */
+function populateModal (employee) {
+
+    // get employee data
+    const img = employee.firstElementChild.src;    
+    const city = employee.querySelector('.city').innerHTML;
+    const name = employee.querySelector('.name').innerHTML;
+    const email = employee.querySelector('.email').innerHTML;    
+    const phone = employee.querySelector('.phone').innerHTML;    
+    const address = employee.querySelector('.address').innerHTML;    
+    const birthday = employee.querySelector('.birthday').innerHTML;  
+    const employeeID = employee.getAttribute('data-employeenumber');  
+      
+    // update and display employee data 
     document.querySelector('.modal .img').src = img;
     document.querySelector('.modal .name').innerHTML = name;
     document.querySelector('.modal .city').innerHTML = city;
@@ -52,11 +112,7 @@ document.querySelector('.panel__main').addEventListener('click', function(evt){
     document.querySelector('.modal .phone').innerHTML = phone;
     document.querySelector('.modal .address').innerHTML = address;
     document.querySelector('.modal .birthday').innerHTML = birthday;
+    document.querySelector('.modal').setAttribute("data-parent", employeeID);
+    document.querySelector('.modal__wrapper').className = "modal__wrapper show";   
 
-    document.querySelector('.modal__wrapper').className = "modal__wrapper show";    
-});   
-
-document.querySelector('.modal__wrapper').addEventListener('click', function(evt){
-    evt.preventDefault();
-    this.className = "modal__wrapper";    
-});      
+}  
